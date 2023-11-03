f979137be32e2762f9b87aab34b871a6821fb1d1
Edward Huang <edward-lemonade@users.noreply.github.com>
2023-11-02 20:42:56 -0700

    add comments to the code

--- ai_funcs.py
+++ ai_funcs.py
@@ -6,6 +6,21 @@ import ckwrap
 import traceback
 import sys
 
+"""
+iCap is an interactive frame capture function used for capturing raw training
+data, or actual data for inference. Press space for capturing, and 'q' for exit.
+
+Parameters:
+ - render: a function adds visual hue into the captured frame
+ - cam: the camera id, defaults to 1
+ - interval: capturing interval, defaults to 10ms
+ - change: a function accepting key stroke to adjust parameters
+
+Return:
+ - ret: the data structure the render function generated based on the frame
+ - frame: the captured frame
+
+"""
 def iCap(render = None, cam = 1, interval = 10, change = None):
     if type(cam) == int:
         cap = cv2.VideoCapture(cam, cv2.CAP_DSHOW)
@@ -47,6 +62,21 @@ def iCap(render = None, cam = 1, interval = 10, change = None):
         cv2.destroyAllWindows()
         cap.release()
 
+"""
+CannyEdges is a wrapper function to invoke cv2 Canny function to generate a
+frame with only canny edges from the source image. It includes steps such as
+turning the image into grayscale, then blur, calculate the gradient. Then
+ignore details without obvious brightness change.
+
+Parameters:
+ - frame: source image
+ - sigma: the scale how much the details are simplified
+ - blur: how blury before using the canny edge algorithm
+
+Returns:
+ - image: the blurred grayscale image
+ - canny: the image with only canny edges
+"""
 def CannyEdges(frame, sigma=0.33, blur=5):
     # step 1 gray blur to reduce noise
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -58,11 +88,29 @@ def CannyEdges(frame, sigma=0.33, blur=5):
     upper = int(min(255, (1.0 + sigma) * gradient))
     return image, cv2.Canny(image, lower, upper)
 
+"""
+HoughLine function is a wrapper to cv2 HoughLines function. It detects
+straightlines from the given canny edge image, and return a list of lines in
+(rho, theta) notion.
+
+Parameters:
+ - edges: the canny edge image
+ - thetaRes: the theta resolution
+ - rhoRes: the rho resolution
+ - minLen: minimun required length of the line
+ - maxGap: maximum allowed gap in the line
+
+Returns:
+ - lines: two dimension array, each row is a pair of rho and theta.
+"""
 def HoughLine(edges, thetaRes = 1, rhoRes = 1, minLen=140, maxGap=10):
     lines = cv2.HoughLines(edges, rhoRes, thetaRes * pi / 180, minLen, maxGap)
     lines = reshape(lines, (-1, 2))
     return lines
 
+"""
+GridLines split the group of lines into horizontal lines and vertical lines
+"""
 def GridLines(lines):
     h, v = [], []
     for r, t in lines:
@@ -73,6 +121,16 @@ def GridLines(lines):
  
     return h, v
 
+"""
+Intersections calculate all the intersections between two groups of lines
+
+Parameters:
+ - h: horizontal lines
+ - v: vertical lines
+
+Returns:
+ - points: a list of points in (x, y) format
+"""
 def Intersections(h, v):
     points = []
     for rh, th in h:
@@ -83,6 +141,20 @@ def Intersections(h, v):
             points.append((int(x),int(y)))
     return points
 
+"""
+FitToGrid groups the intersection points into eleven rows and each with eleven
+clusters. Take the mean of the coordinates of each cluster, we now have 11x11
+grid line intersections. We use 1 dimension K-Means algorithm implemented in
+ckwrap library to group and cluster the points. This assumes that the grid is
+aligned horizontally.
+
+Parameters:
+ - points: a list of points in (x, y) format
+
+Returns:
+ - grid: a 2 dimension array of points (x, y), rows then columns
+
+"""
 def FitToGrid(points):
     rowLabels = ckwrap.ckmeans(points.T[1], 11).labels
     rows = [ empty([0, 2]) for i in range(11) ]
--- capture.py
+++ capture.py
@@ -17,8 +17,14 @@ HVY=10
 VVX=10
 VVY=70
 
+"""
+showGrid is passed to iCap as a render function to show the grid lines
+in the video to guide the capture.
+"""
 def showGrid(frame):
     global TR, RR, SM, BL, ML, MG
+
+    # below are the steps to detect the grid
     image, edges = CannyEdges(frame, sigma=SM, blur=BL)
     lines = HoughLine(edges, thetaRes=TR, rhoRes=RR, minLen=ML, maxGap=MG)
     h, v = GridLines(lines)
@@ -30,19 +36,24 @@ def showGrid(frame):
     #         cv2.circle(edges, p, radius=5, color=(64,77,255), thickness=3)
     # cv2.imshow("edges", edges)
 
+    # draw the horizontal lines
     for row in grid[1:-1]:
         v = numpy.diff(row[1:-1], axis=0).var(axis=0)
+        # throw error if the variance of the distance between each columns is too big 
         assert v[0] < HVX and v[1] < HVY
         cv2.line(frame, row[1], row[-2], color=(64,77,255), thickness=3)
 
+    # draw the vertical lines
     for col in range(1,10):
         trend = numpy.diff(grid[1:-1,col], axis=0)
         v = numpy.diff(trend, axis=0).var(axis=0)
+        # throw error if the variance of the distance change between rows is too big
         assert v[0] < VVX and v[1] < VVY
         cv2.line(frame, grid[1,col], grid[9,col], color=(64,77,255), thickness=3)
 
     return grid, frame
 
+# load the fine-tuned yolov5 weights.
 model = yolov5.load("best.pt")
 
 # set model parameters
@@ -54,24 +65,36 @@ model.max_det = 40  # maximum number of detections per image
 
 while True:
     g, pic = iCap(render=showGrid, interval=5, cam=1)
+    # when space or 'q' is pressed, iCap returns
     if pic is None:
+        # 'q' is pressed, iCap return None for the pic, quit
         break
 
+    # invoke inferernce
     result = model(pic.copy())
+    # confi is the confidence level of each detected obj
     confi = result.pred[0][:,4]
+    # boxes is the bounding box of each detected obj
     boxes = numpy.array(result.pred[0][:,:4]).astype(int)
-    labels = list(map(lambda x:label_map[x], numpy.array(result.pred[0][:,5]).astype(int))
-                  )
+    # convert the raw label into the FEN character based on a label_map
+    labels = list(map(lambda x:label_map[x], numpy.array(result.pred[0][:,5]).astype(int)))
+    # calculate the estmated bottom center position of the piece
     bc = list(map(lambda b:(int((b[0] + b[2])/2), b[3] - int((b[3]-b[1])/4)) ,boxes))
+    # convert the y coordinate into the board location 0-12 using bisect
     grid_loc = list(map(lambda c:(c[0], bisect.bisect(g.mean(axis=1)[:,1], c[1])),bc))
+    # convert the x coordinate into the board location 0-12 using bisect
     grid_loc = list(map(lambda c:(bisect.bisect(g[c[1],:,0],c[0]), c[1]) , grid_loc))
     print(grid_loc)
 
+    # create an empty board
     board = [ [ (" ",0) for j in range(8) ] for i in range(8)]
+
+    # put the pieces into the board
     for i in range(len(labels)):
         row = grid_loc[i][1] - 2
         col = grid_loc[i][0] - 2
         old, prob = board[row][col]
+        # if multiple pieces detected in the same location, use the one with higher confidence level
         if prob < confi[i]:
             if prob > 0:
                 print(f"overriding at ({row}, {col}) from {old} to {labels[i]}, confi from {prob} to {confi[i]}")
@@ -80,6 +103,7 @@ while True:
             print(f"ignoring at ({row}, {col}) keeping {old} instead of {labels[i]}, confi {prob} vs {confi[i]}")
     print(board)
 
+    # convert the board into FEN format line by line
     lines = []
     for row in numpy.array(board)[:,:,0]:
         blank = 0
@@ -98,6 +122,7 @@ while True:
     FEN = "/".join(lines)
     print(FEN)
 
+    # draw the boxes and piece labels in the picture and show it for validation, press space to continue
     for b in range(len(boxes)):
         cv2.circle(pic, bc[b], radius=5, color=(64,77,255), thickness=4)
         cv2.rectangle(pic, boxes[b,:2], boxes[b,2:], color=(235, 52, 82), thickness=3)
